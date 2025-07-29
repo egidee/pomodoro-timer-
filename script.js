25e7c9758959481604d5f9eda8dcda368225b165
Egide <tegide2024@gmail.com>
2025-07-29 14:53:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const App = () => {
     const mins = Math.floor(seconds / 60);
     const secs = seconds % 60;
 
-    return `${mins.toString().padStart(2, "0")}:\${secs.toString().padStart(2, "0")}`;
+    return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
   };
 
   const resetting = () => {
